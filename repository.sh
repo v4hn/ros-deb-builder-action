f8eac194cd9db3309ddc29a0168e6e98264b7e05
v4hn <me@v4hn.de>
2023-02-02 11:25:24 +0100

    use BRANCH for deploy action

--- repository.sh
+++ repository.sh
@@ -6,15 +6,18 @@ set -ex
 echo "::group::Setup deb repository"
 
 cd /home/runner/apt_repo
+
 apt-ftparchive packages . > Packages
 apt-ftparchive release . > Release
 
 REPOSITORY="$(printf "%s" "$GITHUB_REPOSITORY" | tr / _)"
+REPOSITORY_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH/"
+
 echo '```bash' > README.md
-echo "echo \"deb [trusted=yes] https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO/ ./\" | sudo tee /etc/apt/sources.list.d/$REPOSITORY.list" >> README.md
+echo "echo \"deb [trusted=yes] $REPOSITORY_URL/ ./\" | sudo tee /etc/apt/sources.list.d/$REPOSITORY-$BRANCH.list" >> README.md
 echo "sudo apt update" >> README.md
 echo "sudo apt install python3-rosdep2" >> README.md
-echo "echo \"yaml https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$DEB_DISTRO-$ROS_DISTRO/local.yaml debian\" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY.list" >> README.md
+echo "echo \"yaml $REPOSITORY_URL/local.yaml debian\" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY-$BRANCH.list" >> README.md
 echo "rosdep update" >> README.md
 echo '```' >> README.md
 
