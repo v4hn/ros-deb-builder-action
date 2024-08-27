269ae4cba5ad4285b627c83cc56b2ed79661ca59
v4hn <me@v4hn.de>
2024-08-27 13:33:26 +0200

    provide package and failure lists in readme
    for gh-pages integration

--- repository.sh
+++ repository.sh
@@ -12,7 +12,6 @@ apt-ftparchive release . > Release
 
 REPOSITORY="$(printf "%s" "$GITHUB_REPOSITORY" | tr / _)"
 REPOSITORY_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH"
-[ ! -f Failed.md ] || sed -i "s|@REPOSITORY_URL@|$REPOSITORY_URL|" Failed.md
 
 echo '```bash' > README.md
 echo "echo \"deb [trusted=yes] $REPOSITORY_URL/ ./\" | sudo tee /etc/apt/sources.list.d/$REPOSITORY-$BRANCH.list" >> README.md
@@ -21,5 +20,18 @@ echo "sudo apt install python3-rosdep2" >> README.md
 echo "echo \"yaml $REPOSITORY_URL/local.yaml debian\" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY-$BRANCH.list" >> README.md
 echo "rosdep update" >> README.md
 echo '```' >> README.md
+
+echo "\n## Recently Built\n" >> README.md
+
+for file in $(ls -t *.deb); do
+   echo "- [$file]($REPOSITORY_URL/$file)  " >> README.md
+done
+
+if [ -f Failed.md ]; then
+   sed -i "s|@REPOSITORY_URL@|$REPOSITORY_URL|" Failed.md
+   echo "\n## Recently Failed\n" >> README.md
+
+   cat Failed.md >> README.md
+fi
 
 echo "::endgroup::"
