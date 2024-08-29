6504fefa4041088cca8a9b65b2a7bb9fea69e568
v4hn <me@v4hn.de>
2024-08-29 21:39:57 +0200

    improved formatting for overview table

--- repository.sh
+++ repository.sh
@@ -13,23 +13,39 @@ apt-ftparchive release . > Release
 REPOSITORY="$(printf "%s" "$GITHUB_REPOSITORY" | tr / _)"
 REPOSITORY_URL="https://raw.githubusercontent.com/$GITHUB_REPOSITORY/$BRANCH"
 
+cat <<EOF > _config.yml
+plugins:
+  - jemoji
+EOF
+
 cat <<EOF > README.md
+# A ROS-O deb repository for $BRANCH
+
 ## Install Instructions
 
 \`\`\`bash
-deb [trusted=yes] $REPOSITORY_URL/ ./\" | sudo tee /etc/apt/sources.list.d/$REPOSITORY-$BRANCH.list
+echo "deb [trusted=yes] $REPOSITORY_URL/ ./" | sudo tee /etc/apt/sources.list.d/$REPOSITORY-$BRANCH.list
 sudo apt update
 sudo apt install python3-rosdep2
 echo "yaml $REPOSITORY_URL/local.yaml debian" | sudo tee /etc/ros/rosdep/sources.list.d/1-$REPOSITORY-$BRANCH.list
 rosdep update
+
+# install required packages, e.g.,
+sudo apt install ros-one-desktop-full ros-one-plotjuggler ros-one-navigation [...]
 \`\`\`
 EOF
 
+ARCHITECTURES=$(grep Architecture: Packages | cut -d' ' -f2 | sort -u)
+PKG_CNT=$(grep ^Package: Packages | wc -l)
 
 cat <<EOF >> README.md
 
 ## Build
 
+| Target Distribution | ${BRANCH%-*} |
+| Architecture | $ARCHITECTURES |
+| Available Packages | $PKG_CNT |
+| Built Date | $(date) |
 EOF
 
 # echo "Package,Status,Bloom Log,Build Log,Deb File" > $PKG_STATUS
@@ -40,10 +56,41 @@ if [ -f $PKG_STATUS ]; then
 
 ## Build Status
 
-| Package | Status | Bloom Log | Build Log | Deb File |
-| ------- | ------ | --------- | --------- | -------- |
+|   | Logs | Package |
+| - | ---- | ------- |
 EOF
-   cat $PKG_STATUS | awk -F, -v repo="$REPOSITORY_URL" '{printf "| " $1 " | " $2 " | [bloom-generate](" repo "/" $3 ") | [sbuild](" repo "/" $4 ") | [deb](" repo "/" $5 ") |\n"}' >> README.md
+   cat $PKG_STATUS | awk -F, -v repo="$REPOSITORY_URL" '
+{
+   pkg=$1
+   status=$2
+   bloom_log=repo "/" $3
+   build_log=repo "/" $4
+   deb=repo "/" $5
+
+   if(status == "success") {
+      estatus=":green_circle:"
+      epkg="[" pkg "](" deb ")"
+      ebloom="[:green_book:](" bloom_log ")"
+      ebuild="[:green_book:](" build_log ")"
+   }
+   else if(status == "failed-sbuild") {
+      estatus=":construction:"
+      epkg=pkg
+      ebloom="[:green_book:](" bloom_log ")"
+      ebuild="[:orange_book:](" build_log ")"
+   }
+   else if(status == "failed-bloom-generate"){
+      estatus=":construction:"
+      epkg=pkg
+      ebloom="[:orange_book:](" bloom_log ")"
+      ebuild=""
+   }
+   else {
+      print "unknown status: " status
+      exit 1
+   }
+   printf "| " estatus " | " ebloom " " ebuild " | " epkg " |\n"
+}' >> README.md
 fi
 
 echo "::endgroup::"
